b8fd97f01c8c7326330ac23addc7808af0d6e252
jonivalor <jonivalor@gmail.com>
2022-01-31 10:04:04 -0300

    actualizacion

--- dyno-client.js
+++
@@ -1,4 +0,0 @@
-
-// dyno-client.js
-require('dotenv').config()
-const { MY_AWS_ACCESS_KEY_ID, MY_AWS_SECRET_ACCESS_KEY, MY_AWS_REGION } = process.env
--- functions/shopify-get-orders/shopify-get-orders.mjs
+++ functions/shopify-get-orders/shopify-get-orders.mjs
@@ -1,16 +1,13 @@
 import fetch from 'node-fetch';
 
-require('dotenv').config()
-const { STORE_URL, TOKEN } = process.env
-
-const API_ENDPOINT = `${STORE_URL}/admin/api/2021-10/orders.json`;
+const API_ENDPOINT = `https://punitastore.myshopify.com/admin/api/2021-10/orders.json`;
 
 exports.handler = async (event, context) => {
   try {
     const response = await fetch(API_ENDPOINT, {
         headers: {
           'Content-Type': 'application/json',
-          'X-Shopify-Access-Token': `${TOKEN}`,
+          'X-Shopify-Access-Token': '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',
         },
       });
     const data = await response.json();
